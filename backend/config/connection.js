16f40214a35a5911e88385fd104d14a1e263cfe0
Debashish Karmakar <debashishkarmakar326@gmail.com>
2026-04-08 19:51:12 +0530

    Update connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectMongoDb(url) {
+    console.log("Mongo URL:", url);
     return mongoose.connect(`${url}/ETech`)
         .then(() => console.log("MongoDB connected"))
         .catch(err => console.log(err));
