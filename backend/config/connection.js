e2b044e53e5d69659d029c57ce7e94b4de482614
Debashish Karmakar <debashishkarmakar326@gmail.com>
2026-04-08 19:44:15 +0530

    Refactor MongoDB connection function

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connectMongoDb(url) {
-    return mongoose.connect(`${url}/E-Tech`).then(console.log("Mongo Db connected"))
+    return mongoose.connect(`${url}/ETech`)
+        .then(() => console.log("MongoDB connected"))
+        .catch(err => console.log(err));
 }
-module.exports={connectMongoDb}
 
-const db = mongoose.connection;
+module.exports = { connectMongoDb };
